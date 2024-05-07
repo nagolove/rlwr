b41e82e18d769d84e8cbd7fc3ab1f08a4a599162
nagolove <dekar.own@gmail.com>
2024-05-08 02:09:44 +0300

    linking

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env bash
 
 # TODO: Поменять версию Луа. Использовать 3rdparty библиотеки.
-g++ -c rlwr.cpp -fPIC -std=c++2a -I. -Isol -I/usr/include/lua5.3 -lm -llua5.3 -lraylib -Iraylib/raylib/include -Lraylib/raylib/
+g++ -c rlwr.cpp                 \
+    -fPIC -std=c++2a            \
+    -I.                         \
+    -Isol                       \
+    -I../lua                    \
+    -lm                         \
+    -llua                       \
+    -lraylib                    \
+    -I../raylib/raylib/include  \
+    -L../raylib/raylib/         \
+
+ar rcs librlwr.a rlwr.o
+
