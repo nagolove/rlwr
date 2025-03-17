85d5c7dcee3e5fd0f657f8232db176234a28938a
nagolove <dekar.own@gmail.com>
2025-03-17 23:36:33 +0300

    Не собирается для wasm

--- build.sh
+++ build.sh
@@ -20,25 +20,40 @@ if [[ "$TARGET" == "linux" ]]; then
     echo "Компиляция для Linux..."
     KOMPILER="g++"
     LINKER="ar"
+
+
+    $KOMPILER -c rlwr.cpp \
+        -I.                            \
+        -I../sol2/single/include       \
+        -L../lua                       \
+        -I../lua                       \
+        -I../raylib/raylib/include     \
+        -L../raylib/raylib/            \
+        -lm                            \
+        -llua                          \
+        -lraylib                       \
+        -o rlwr.o                      \
+
+    $LINKER rcs librlwr.a rlwr.o
+
 elif [[ "$TARGET" == "wasm" ]]; then
     echo "Компиляция для wasm..."
     KOMPILER="em++"
     LINKER="emar"
+
+    $KOMPILER -c rlwr.cpp \
+        -I.                            \
+        -I../sol2/single/include       \
+        -I../lua                       \
+        -I../raylib/raylib/include     \
+        ../raylib/raylib/libraylib.a   \
+        ../lua/liblua.a                \
+        -o rlwr.o                      \
+
+    $LINKER rcs librlwr.a rlwr.o
+
 else
     echo "Неизвестная цель: $TARGET"
     exit 1
 fi
 
-$KOMPILER -c rlwr.cpp \
-    -I.                            \
-    -I../sol2/single/include       \
-    -L../lua                       \
-    -I../lua                       \
-    -I../raylib/raylib/include     \
-    -L../raylib/raylib/            \
-    -lm                            \
-    -llua                          \
-    -lraylib                       \
-    -o rlwr.o                      \
-
-$LINKER rcs librlwr.a rlwr.o
