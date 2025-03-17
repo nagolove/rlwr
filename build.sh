0f3eb39951e5a64a6a4c670f3b7df1d4a6785a47
nagolove <dekar.own@gmail.com>
2025-03-17 22:54:31 +0300

    update

--- build.sh
+++ build.sh
@@ -12,5 +12,17 @@ g++ -c rlwr.cpp                 \
     -I../raylib/raylib/include  \
     -L../raylib/raylib/         \
 
+g++ -c rlwr.cpp                 \
+    -fPIC -std=c++2a            \
+    -I.                         \
+    -I../sol2/single/include    \
+    -L../lua                    \
+    -I../lua                    \
+    -I../raylib/raylib/include  \
+    -L../raylib/raylib/         \
+    -lm                         \
+    -llua                       \
+    -lraylib                    \
+
 ar rcs librlwr.a rlwr.o
 
