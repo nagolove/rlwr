31222690f8c98f68d65a4dcdb4c72351117ce06f
nagolove <dekar.own@gmail.com>
2025-03-17 23:20:52 +0300

    upd

--- build.sh
+++ build.sh
@@ -1,16 +1,44 @@
 #!/usr/bin/env bash
 
-g++ -c rlwr.cpp                 \
-    -fPIC -std=c++2a            \
-    -I.                         \
-    -I../sol2/single/include    \
-    -L../lua                    \
-    -I../lua                    \
-    -I../raylib/raylib/include  \
-    -L../raylib/raylib/         \
-    -lm                         \
-    -llua                       \
-    -lraylib                    \
+# Значение по умолчанию
+TARGET="linux"
 
-ar rcs librlwr.a rlwr.o
+# Обработка аргументов командной строки
+for arg in "$@"; do
+    case $arg in
+        target=*) TARGET="${arg#*=}" ;;
+    esac
+done
 
+echo "Сборка для $TARGET..."
+
+KOMPILER="unknown_command"
+LINKER="unknown_command"
+
+# Проверяем значение TARGET
+if [[ "$TARGET" == "linux" ]]; then
+    echo "Компиляция для Linux..."
+    KOMPILER="g++"
+    LINKER="ar"
+elif [[ "$TARGET" == "wasm" ]]; then
+    echo "Компиляция для wasm..."
+    KOMPILER="emcc"
+    LINKER="emar"
+else
+    echo "Неизвестная цель: $TARGET"
+    exit 1
+fi
+
+$KOMPILER -c rlwr.cpp \
+    -I.                            \
+    -I../sol2/single/include       \
+    -L../lua                       \
+    -I../lua                       \
+    -I../raylib/raylib/include     \
+    -L../raylib/raylib/            \
+    -lm                            \
+    -llua                          \
+    -lraylib                       \
+    -o rlwr.o                      \
+
+$LINKER rcs librlwr.a rlwr.o
